5c5b3c8ac547a80bd376df23ae99d7709777d837
Greenwoodsy <greenwoodsy@zohomail.com>
2025-10-01 21:30:13 +0100

    updaste

--- server.js
+++ server.js
@@ -1,39 +1,11 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const passport = require('passport');      
-const session = require('express-session'); 
-const connectDB = require('./config/db');
+const express = require('express')
+const dotenv = require('dotenv')
 
-dotenv.config();
-connectDB();
+dotenv.config()
 
-const app = express();
+const app = express()
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
-app.use(express.json());
-app.use(cookieParser());
-
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET || 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      secure: false,
-      maxAge: 1000 * 60 * 10,
-    },
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Auth routes
-app.use('/auth', require('./routes/microsoftAuth'));
-
-const PORT = process.env.PORT || 5200;
+const PORT = process.env.PORT || 5200
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
