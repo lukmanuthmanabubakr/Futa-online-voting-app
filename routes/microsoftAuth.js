104d97e7eafa2326c7ef7c01c72cbfba0e6a8e4b
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2026-02-22 23:25:23 +0100

    update

--- routes/microsoftAuth.js
+++ routes/microsoftAuth.js
@@ -23,9 +23,6 @@ passport.deserializeUser(async (id, done) => {
 //     {
 //       identityMetadata: `https://login.microsoftonline.com/${process.env.AZURE_TENANT_ID}/v2.0/.well-known/openid-configuration`,
 //       clientID: process.env.AZURE_CLIENT_ID,
-//       clientSecret: process.env.AZURE_CLIENT_SECRET,
-//       responseType: "code",
-//       responseMode: "query",
 //       redirectUrl: process.env.AZURE_REDIRECT_URI,
 //       allowHttpForRedirectUrl: true,
 //       passReqToCallback: false,
